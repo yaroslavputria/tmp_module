55e19762e6d2c1b8108994dfc6b25f7000ccba46
Yaroslav Putria <yaroslav.putria@gmail.com>
2016-12-06 11:31:36 +0200

    firts

---
+++ server.js
@@ -0,0 +1,8 @@
+let http = require('http');
+let express = require('express');
+
+http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello world\n');
+}).listen(8081);
+console.log(express);
